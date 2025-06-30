4c64b0449d6cb8b7d039ff60331004dfa22a2184
curls-2090 <144624053+curls-2090@users.noreply.github.com>
2025-07-01 00:43:22 +0630

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+// index.js
+const express = require('express');
+const app = express();
+const { exec } = require('child_process');
+
+app.use(express.static('public'));
+
+app.get('/cowsay/:message', (req, res) => {
+  exec(`/usr/games/cowsay ${req.params.message}`, { timeout: 5000 }, (error, stdout) => {
+    if (error) return res.status(500).end();
+    res.type('txt').send(stdout).end();
+  });
+});
+
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+});
+
+    
